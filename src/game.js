c5bca1c3cd5ff92cb65baab471482f937d42e42f
Anand Patel <anandkumarpatel@gmail.com>
2023-07-30 14:39:09 -0400

    update game to hooks and autosize

--- src/game.js
+++ src/game.js
@@ -1,11 +1,12 @@
 import cx from 'classnames'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { isIOS } from 'react-device-detect'
 import './game.scss'
 import { hash } from './images'
 import countdownSoundFile from './sounds/countdown.mp3'
 import correctSound from './sounds/sound_correct.mp3'
 import skipSound from './sounds/sound_skip.mp3'
+import { AutoTextSize } from 'auto-text-size'
 
 const removeUnderscores = (str) => str.replace(/_/g, ' ')
 
@@ -13,67 +14,64 @@ const toMMSS = (s) => (s - (s %= 60)) / 60 + (9 < s ? ':' : ':0') + s
 
 const countdownStartTime = isIOS ? 12 : 11
 
-class Game extends React.Component {
-  componentDidUpdate(prevProps) {
-    if (this.props.enableSoundEffects && prevProps.activeItem !== this.props.activeItem && prevProps.activeItem !== '') {
-      if (prevProps.isAnimating === 'correct') {
-        this.props.soundFile.src = correctSound
-        this.props.soundFile.play()
+const Game = ({ activeCollection, inGameTimer, activeItem, isAnimating, removeAnimationClasses, enableSoundEffects }) => {
+  const countdownSound = new Audio(countdownSoundFile)
+  const soundFile = new Audio()
+
+  useEffect(() => {
+    if (enableSoundEffects && activeItem !== '' && activeItem !== undefined) {
+      if (isAnimating === 'correct') {
+        soundFile.src = correctSound
+        soundFile.play()
       }
-      if (prevProps.isAnimating === 'skip') {
-        this.props.soundFile.src = skipSound
-        this.props.soundFile.play()
+      if (isAnimating === 'skip') {
+        soundFile.src = skipSound
+        soundFile.play()
       }
     }
-    if (prevProps.inGameTimer === countdownStartTime) {
-      this.props.countdownSound.src = countdownSoundFile
-      this.props.countdownSound.play()
+    if (inGameTimer === countdownStartTime) {
+      countdownSound.src = countdownSoundFile
+      countdownSound.play()
     }
+  }, [activeItem, isAnimating, inGameTimer, enableSoundEffects])
+
+  const getActiveItem = () => {
+    return activeCollection.name.toLowerCase().includes('kids') ? <img src={hash[activeItem]} alt={removeUnderscores(activeItem)} className='Game-image' /> : activeItem
   }
 
-  render() {
-    const { activeCollection, inGameTimer, activeItem, isAnimating, removeAnimationClasses } = this.props
+  const lessThanTen = inGameTimer < 10
+  const timer = toMMSS(inGameTimer)
+  const statusText = isAnimating === 'correct' ? 'Correct!' : 'Skip!'
 
-    const lessThanTen = inGameTimer < 10
-    const timer = toMMSS(inGameTimer)
-    const statusText = isAnimating === 'correct' ? 'Correct!' : 'Skip!'
-
-    const getActiveItem = () => {
-      return activeCollection.name.toLowerCase().includes('kids') ? <img src={hash[activeItem]} alt={removeUnderscores(activeItem)} className='Game-image' /> : activeItem
-    }
-    const style = {}
-    if (activeCollection.name.toLowerCase().includes('emoji')) {
-      style['font-size'] = '12rem'
-    }
-    return (
-      <>
-        <div
-          className={cx('Staging', {
-            'Staging--correct': isAnimating === 'correct',
-            'Staging--skip': isAnimating === 'skip',
-          })}
-          onTransitionEnd={removeAnimationClasses}
-        >
-          {activeItem !== undefined ? (
-            <div style={style} className='Game-activeItem'>
-              {isAnimating !== '' ? statusText : getActiveItem()}
-            </div>
-          ) : (
-            <p>
-              Category is empty :( <br /> Carefully shake device to return to menu.
-            </p>
-          )}
-          <div
-            className={cx('Timer', {
-              'is-countdown': lessThanTen,
-            })}
-          >
-            {timer}
+  return (
+    <>
+      <div
+        className={cx('Staging', {
+          'Staging--correct': isAnimating === 'correct',
+          'Staging--skip': isAnimating === 'skip',
+        })}
+        style={{ height: '100vh', margin: '0 auto' }}
+        onTransitionEnd={removeAnimationClasses}
+      >
+        {activeItem !== undefined ? (
+          <div style={{ height: '90vh', margin: '0 auto' }}>
+            <AutoTextSize mode='box'>{isAnimating !== '' ? statusText : getActiveItem()}</AutoTextSize>
           </div>
+        ) : (
+          <p>
+            Category is empty :( <br /> Carefully shake device to return to menu.
+          </p>
+        )}
+        <div
+          className={cx('Timer', {
+            'is-countdown': lessThanTen,
+          })}
+        >
+          {timer}
         </div>
-      </>
-    )
-  }
+      </div>
+    </>
+  )
 }
 
 export default Game
